37658dada9c079a718893cbd770e98997836d984
romulosmarques <romulo.s.marques@gmail.com>
2024-06-04 21:55:35 -0300

    Returning the number of bt nodes, infeasible calls and distances

--- run_tests.py
+++ run_tests.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from fbs.algorithms import FBS, DFS, DDGP, bp
+import timeit
 
 
 def process_file(fn, n, states, p, verbose=False):
@@ -24,12 +25,19 @@ def process_file(fn, n, states, p, verbose=False):
 
     statistic = []
 
+    n_experiments = 20
     item = {"fn": fn, "n": n, "method": "dfs"}
     try:
+        # mytime = timeit.timeit(stmt="bp(D, dfs)", number=n_experiments)
+        # item["time_secs"] = mytime / n_experiments
         tic = time.time()
-        bp(D, dfs)
+        _, n_infeasible, n_constraints = bp(D, dfs)
         toc = time.time() - tic
         item["time_secs"] = toc
+        item["n_bt_nodes"] = dfs.n_bt_nodes
+        item["n_infeasible"] = n_infeasible
+        item["n_constraints"] = n_constraints
+        item["n_one_nodes"] = sum(dfs.T[4:])/(dfs.n - 4)
     except:
         item["time_secs"] = None
     statistic.append(item)
@@ -39,12 +47,18 @@ def process_file(fn, n, states, p, verbose=False):
     if verbose:
         print(f"DFS: {item['time_secs']:.2E} secs")
 
+    D.n_infeasible = 0
     item = {"fn": fn, "n": n, "method": "fbs"}
     try:
+        # mytime = timeit(stmt="bp(D, fbs)", number=n_experiments)
+        # item["time_secs"] = mytime / n_experiments
         tic = time.time()
-        bp(D, fbs)
+        _, n_infeasible, n_constraints = bp(D, fbs)
         toc = time.time() - tic
         item["time_secs"] = toc
+        item["n_bt_nodes"] = fbs.n_bt_nodes
+        item["n_infeasible"] = n_infeasible
+        item["n_constraints"] = n_constraints
     except:
         item["time_secs"] = None
     statistic.append(item)
@@ -86,6 +100,7 @@ def ddgp_samples(ntests, df_tests):
     random.seed(42)
 
     df_sample = []
+    df_tests = df_tests[df_tests['bin'] >= 3]
     for bin in df_tests["bin"].unique():
         df_tests_bin = df_tests[df_tests["bin"] == bin]
         if len(df_tests_bin) < ntests:
@@ -117,7 +132,7 @@ def read_states():
 def test_instance():
     states, p = read_states()
 
-    fn = '2kb9_model1_chainA_segment4'
+    fn = '2jxc_model1_chainA_segment2'
     fn_bsol = os.path.join('bsol', fn + '_binary.csv')
     fn_ddgp = os.path.join('ddgp', fn + '_ddgp.pkl')    
     
@@ -127,15 +142,17 @@ def test_instance():
     with open(fn_ddgp, 'rb') as f:
         D = pickle.load(f)
     
-    fbs = FBS(D, states, p)
-    bp(D, fbs, verbose=True)
+    dfs = DFS(D)
+    bp(D, dfs, verbose=True)
+    # fbs = FBS(D, states, p)
+    # bp(D, fbs, verbose=True)
 
 def main():
     states, p = read_states()
 
     df_ddgp_bins = set_ddgp_bins()
 
-    df_ddgp_sample = ddgp_samples(5, df_ddgp_bins)
+    df_ddgp_sample = ddgp_samples(10, df_ddgp_bins)
 
     statistic = []
     for _, row in tqdm(list(df_ddgp_sample.iterrows())):
